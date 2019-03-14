690d12eaf6b992c2bf0df84804c1ff5a5bbc8a25
Tomer Keren <tomer.keren.dev@gmail.com>
2019-03-14 12:45:10 +0200

    Open in editor

--- repo_link/repo_link.py
+++ repo_link/repo_link.py
@@ -1,6 +1,10 @@
+import os
+import subprocess
 import re
-from typing import Tuple, Optional
+from contextlib import contextmanager
 from pathlib import Path
+from typing import Iterator, Optional, Tuple, Union
+from git import Repo
 
 
 def parse(link: str) -> Tuple[Path, str, Optional[str]]:
@@ -15,16 +19,40 @@ def parse(link: str) -> Tuple[Path, str, Optional[str]]:
     raise ValueError("Invalid link")
 
 
-def checkout(path: str):
+def checkout(path: Path, commit: str):
     """Checks out the given github pathspec (commit/branch) in the local repository"""
+    repo = Repo(path)
+    repo.git.checkout(commit)
 
 
 def clone(parent_dir: Path, repo_path: str):
     """Clones the given repo in parent_dir. Raises FileAlreadyExists if the repo exists"""
 
 
+def open_in_editor(path: Path, line: str, editor=os.environ["EDITOR"]):
+    """Opens the file given in path and line, inside the editor"""
+    command = {
+        "vim": f"{editor} +{line} {path}",
+        "code": f"{editor} -g {path}:{line}",
+        "pycharm": f"{editor} {path}:{line}",
+    }[editor.strip(".exe")]
+    subprocess.run(command.split())
+
+
+PathType = Union[os.PathLike, str]
+
+
+@contextmanager
+def cd(path: PathType) -> Iterator[None]:
+    old_path = Path.cwd()
+    os.chdir(path)
+    yield
+    os.chdir(old_path)
+
+
 def main():
-    pass
+    FILE_PATH = Path(__file__)
+    open_in_editor(FILE_PATH, 4)
 
 
 if __name__ == "__main__":
